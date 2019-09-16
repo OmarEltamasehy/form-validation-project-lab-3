61f670d9fd5332f09b7c212eeb327d2d5a4ad10b
Macintosh HD <omareltamasehy@gmail.com>
2019-09-16 22:09:08 +0200

    form validation is developed well

---
+++ validation.js
@@ -0,0 +1,34 @@
+
+        let list = []
+        let name = document.getElementById('name');
+        let mail = document.getElementById('mail');
+        let pass = document.getElementById('pass');
+
+        let gender   = document.getElementsByName('gender');
+        let country  = document.getElementById('country')
+        
+        function validate(){
+            list = []
+            if(pass.value.length < 8){
+                list.push("password must be at least 8 character");
+            }
+            if(name.value.length == "" || mail.value.length == "" || pass.value.length == ""){
+                list.push("All fields Required")
+            }
+
+            if ( ( gender[0].checked == false ) && ( gender[1].checked == false ) )
+            {
+                list.push( "Please choose your Gender: Male or Female" );
+            }
+
+            if ( country.value == country.options[0].value)
+            {
+                list.push( "Please choose your country" );
+            }
+            
+            console.log(list);
+        } 
+
+        btn.addEventListener('click' , validate);
+        //validate()
+
